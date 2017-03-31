d6f0ac5cc8c1ca6d769e34a3bd3d5fdbddc1bce0
Gary Hale <gary@gradle.com>
2017-03-31 18:03:59 -0400

    Add basic deadlock detection to resource lock coordinator

--- subprojects/base-services/src/main/java/org/gradle/internal/resources/DefaultResourceLockCoordinationService.java
+++ subprojects/base-services/src/main/java/org/gradle/internal/resources/DefaultResourceLockCoordinationService.java
@@ -17,13 +17,19 @@
 package org.gradle.internal.resources;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.gradle.api.Transformer;
+import org.gradle.api.specs.Spec;
+import org.gradle.internal.Pair;
 import org.gradle.internal.UncheckedException;
+import org.gradle.util.CollectionUtils;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class DefaultResourceLockCoordinationService implements ResourceLockCoordinationService {
@@ -34,25 +40,24 @@ public class DefaultResourceLockCoordinationService implements ResourceLockCoord
             return Lists.newArrayList();
         }
     };
+    private final Map<Thread, DefaultResourceLockState> waiting = Maps.newHashMap();
+    private final Set<ResourceLockRegistry> lockRegistries = Sets.newConcurrentHashSet();
 
     @Override
-    public boolean withStateLock(Transformer<ResourceLockState.Disposition, ResourceLockState> reasourceLockAction) {
+    public boolean withStateLock(Transformer<ResourceLockState.Disposition, ResourceLockState> resourceLockAction) {
         while (true) {
             DefaultResourceLockState resourceLockState = new DefaultResourceLockState();
             ResourceLockState.Disposition disposition;
             synchronized (lock) {
                 try {
                     currentState.get().add(resourceLockState);
-                    disposition = reasourceLockAction.transform(resourceLockState);
+                    disposition = resourceLockAction.transform(resourceLockState);
 
                     switch (disposition) {
                         case RETRY:
                             releaseLocks(resourceLockState);
-                            try {
-                                lock.wait();
-                            } catch (InterruptedException e) {
-                                throw UncheckedException.throwAsUncheckedException(e);
-                            }
+                            checkForDeadlocks(resourceLockState);
+                            waitForLockStateChange(resourceLockState);
                             break;
                         case FINISHED:
                             return true;
@@ -82,11 +87,27 @@ public class DefaultResourceLockCoordinationService implements ResourceLockCoord
         }
     }
 
+    private void waitForLockStateChange(DefaultResourceLockState resourceLockState) {
+        waiting.put(Thread.currentThread(), resourceLockState);
+        try {
+            lock.wait();
+        } catch (InterruptedException e) {
+            throw UncheckedException.throwAsUncheckedException(e);
+        } finally {
+            waiting.remove(Thread.currentThread());
+        }
+    }
+
     @Override
     public void notifyStateChange() {
         synchronized (lock) {
             lock.notifyAll();
         }
+    }
+
+    @Override
+    public void addRegistry(ResourceLockRegistry registry) {
+        lockRegistries.add(registry);
     }
 
     private void releaseLocks(DefaultResourceLockState stateLock) {
@@ -95,16 +116,150 @@ public class DefaultResourceLockCoordinationService implements ResourceLockCoord
         }
     }
 
+    private void checkForDeadlocks(DefaultResourceLockState resourceLockState) {
+        Set<ResourceLock> locksIHold = getResourceLocksByThread(Thread.currentThread());
+        Map<ResourceLock, Boolean> alreadyChecked = Maps.newHashMap();
+        Set<Pair<Thread, Iterable<ResourceLock>>> cycle = Sets.newLinkedHashSet();
+        for (ResourceLock failedLock : resourceLockState.getResourceLockFailures()) {
+            if (checkForCycle(cycle, locksIHold, failedLock, alreadyChecked)) {
+                cycle.add(Pair.of(Thread.currentThread(), getHolderLocks(failedLock)));
+                List<Pair<Thread, Iterable<ResourceLock>>> cycleList = Lists.newArrayList(cycle);
+                Collections.reverse(cycleList);
+                throw new ResourceDeadlockException(cycleList);
+            }
+        }
+    }
+
+    private boolean checkForCycle(final Set<Pair<Thread, Iterable<ResourceLock>>> cycle, final Set<ResourceLock> locksIHold, final ResourceLock failedLock, Map<ResourceLock, Boolean> alreadyChecked) {
+        if (alreadyChecked.containsKey(failedLock)) {
+            return alreadyChecked.get(failedLock);
+        }
+
+        boolean cycleExists = false;
+        if (failedLock instanceof LeaseHolder) {
+            cycleExists = checkForLeaseHolderCycle(cycle, locksIHold, failedLock, alreadyChecked);
+        } else if (waiting.containsKey(failedLock.getOwner())) {
+            DefaultResourceLockState ownerLockState = waiting.get(failedLock.getOwner());
+            for (ResourceLock ownerFailedLock : ownerLockState.getResourceLockFailures()) {
+                cycleExists = hasCycle(cycle, locksIHold, failedLock.getOwner(), ownerFailedLock, alreadyChecked);
+                if (cycleExists) {
+                    break;
+                }
+            }
+        }
+
+        alreadyChecked.put(failedLock, cycleExists);
+        return cycleExists;
+    }
+
+    private boolean checkForLeaseHolderCycle(final Set<Pair<Thread, Iterable<ResourceLock>>> cycle, final Set<ResourceLock> locksIHold, final ResourceLock failedLock, final Map<ResourceLock, Boolean> alreadyChecked) {
+        final LeaseHolder root = ((LeaseHolder) failedLock).getRoot();
+
+        // find all of the waiting threads that hold leases for the same root
+        Map<Thread, DefaultResourceLockState> waitersWithLeases = getWaitersWithLeaseRoot(root, locksIHold);
+
+        // If all of the leases are being held by waiting threads
+        if (waitersWithLeases.size() >= root.getMaxLeases()) {
+            // Check to see if all of the waiters that hold leases are waiting on a lock I hold
+            return CollectionUtils.every(waitersWithLeases.entrySet(), new Spec<Map.Entry<Thread, DefaultResourceLockState>>() {
+                @Override
+                public boolean isSatisfiedBy(final Map.Entry<Thread, DefaultResourceLockState> waiter) {
+                    return CollectionUtils.any(waiter.getValue().getResourceLockFailures(), new Spec<ResourceLock>() {
+                        @Override
+                        public boolean isSatisfiedBy(ResourceLock waiterFailedLock) {
+                            return hasCycle(cycle, locksIHold, waiter.getKey(), waiterFailedLock, alreadyChecked);
+                        }
+                    });
+                }
+            });
+        } else {
+            return false;
+        }
+    }
+
+    private boolean hasCycle(final Set<Pair<Thread, Iterable<ResourceLock>>> cycle, final Set<ResourceLock> locksIHold, Thread thread, final ResourceLock failedLock, Map<ResourceLock, Boolean> alreadyChecked) {
+        boolean leaseHolderCycle = failedLock instanceof LeaseHolder && containsLeaseRoot(locksIHold, ((LeaseHolder)failedLock).getRoot());
+
+        if (leaseHolderCycle || locksIHold.contains(failedLock) || checkForCycle(cycle, locksIHold, failedLock, alreadyChecked)) {
+            cycle.add(Pair.of(thread, getHolderLocks(failedLock)));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private Map<Thread, DefaultResourceLockState> getWaitersWithLeaseRoot(final LeaseHolder root, Set<ResourceLock> locksIHold) {
+        Map<Thread, DefaultResourceLockState> waitersWithLeases = CollectionUtils.filter(waiting, new Spec<Map.Entry<Thread, DefaultResourceLockState>>() {
+            @Override
+            public boolean isSatisfiedBy(final Map.Entry<Thread, DefaultResourceLockState> waiter) {
+                Set<ResourceLock> waiterLocks = getResourceLocksByThread(waiter.getKey());
+                return containsLeaseRoot(waiterLocks, root);
+            }
+        });
+
+        if (containsLeaseRoot(locksIHold, root)) {
+            waitersWithLeases.put(Thread.currentThread(), (DefaultResourceLockState)getCurrent());
+        }
+
+        return waitersWithLeases;
+    }
+
+    private boolean containsLeaseRoot(Set<ResourceLock> locks, final LeaseHolder root) {
+        return CollectionUtils.any(locks, new Spec<ResourceLock>() {
+            @Override
+            public boolean isSatisfiedBy(ResourceLock resourceLock) {
+                return resourceLock instanceof LeaseHolder && ((LeaseHolder)resourceLock).getRoot() == root;
+            }
+        });
+    }
+
+    private Iterable<ResourceLock> getHolderLocks(ResourceLock resourceLock) {
+        if (resourceLock instanceof LeaseHolder) {
+            Set<ResourceLock> holderLocks = Sets.newHashSet();
+            final LeaseHolder root = ((LeaseHolder) resourceLock).getRoot();
+            Set<Thread> threads = Sets.newHashSet(waiting.keySet());
+            threads.add(Thread.currentThread());
+            for (Thread waitingThread : threads) {
+                for (ResourceLock lock : getResourceLocksByThread(waitingThread)) {
+                    if (lock instanceof LeaseHolder && ((LeaseHolder)lock).getRoot() == root) {
+                        holderLocks.add(lock);
+                    }
+                }
+            }
+            return holderLocks;
+        } else {
+            return Lists.newArrayList(resourceLock);
+        }
+    }
+
+    private Set<ResourceLock> getResourceLocksByThread(Thread thread) {
+        Set<ResourceLock> resourceLocks = Sets.newHashSet();
+        for (ResourceLockRegistry registry : lockRegistries) {
+            resourceLocks.addAll(registry.getResourceLocksByThread(thread));
+        }
+        return resourceLocks;
+    }
+
     private static class DefaultResourceLockState implements ResourceLockState {
         private final Set<ResourceLock> resourceLocks = Sets.newHashSet();
+        private final Set<ResourceLock> resourceLockFailures = Sets.newHashSet();
 
         @Override
         public void registerLocked(ResourceLock resourceLock) {
             resourceLocks.add(resourceLock);
         }
 
+        @Override
+        public void registerFailed(ResourceLock resourceLock) {
+            resourceLockFailures.add(resourceLock);
+        }
+
         Set<ResourceLock> getResourceLocks() {
             return resourceLocks;
+        }
+
+        Set<ResourceLock> getResourceLockFailures() {
+            return resourceLockFailures;
         }
     }
 
